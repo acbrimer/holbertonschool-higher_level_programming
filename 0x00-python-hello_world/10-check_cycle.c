c5562345173cb0d5c125331f3a864d0a2c4bc7d8
Adam Brimer <abrimer@mac.com>
2021-04-26 17:03:39 +0000

    Added solution for task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if linked list has a cycle
+ * @list: starting node for linked list
+ *
+ * Return: 1 for cycle, else 0
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *tmp = list;
+	listint_t *head = list;
+
+	while (tmp)
+	{
+		if (tmp->next == head)
+			return (1);
+		tmp = tmp->next;
+	}
+
+	return (0);
+}
