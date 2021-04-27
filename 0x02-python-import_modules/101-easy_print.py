eb4c69d5d5a1fa96cd6eeae1497de8a8362359ee
Adam Brimer <abrimer@mac.com>
2021-04-27 23:10:53 +0000

    Fix for 101

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import os
 if __name__ == '__main__':
-	outfd = os.fdopen(1, 'w')
-	outfd.write("#pythoniscool\n")
-	outfd.close()
+    outfd = os.fdopen(1, 'w')
+    outfd.write("#pythoniscool\n")
+    outfd.close()
