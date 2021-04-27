21ac39e1325959ac0cc06e1b4510559ec1388ea4
Adam Brimer <abrimer@mac.com>
2021-04-27 21:21:52 +0000

    Fix for 0-- check for '__main__'

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-math = __import__('add_0', ['add'])
-add = math.add
-a = 1
-b = 2
-print("{0} + {1} = {2}".format(a, b, add(a, b)))
+from add_0 import add
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
