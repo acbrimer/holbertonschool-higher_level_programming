fe255500deacef35d1053e0c2b61a615b807c9a3
Adam Brimer <abrimer@mac.com>
2021-04-27 22:07:10 +0000

    Fix for task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from inspect import getmembers, isfunction
 import hidden_4
-for m in getmembers(hidden_4, isfunction):
-    if m[0][0:2] != "__":
-        print("{}".format(m[0]))
+if __name__ == '__main__':
+    for m in getmembers(hidden_4, isfunction):
+        if m[0][0:2] != "__":
+            print("{}".format(m[0]))
