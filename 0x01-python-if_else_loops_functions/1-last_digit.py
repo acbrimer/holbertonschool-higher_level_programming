cd78cf79a2c247d36b01aa7d66b797109dbd3047
Adam Brimer <abrimer@mac.com>
2021-04-27 18:52:14 +0000

    Fix for task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+last_digit = (number % 10) * (1, -1)[number <= 0]
 print_str = "Last digit of {0} is {1} and is".format(number, last_digit)
 if last_digit == 0:
     print("{} zero".format(print_str))
