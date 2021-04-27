b5d02914e83d00d8ea6b9f0a0fb6b931fa011a32
Adam Brimer <abrimer@mac.com>
2021-04-27 18:50:18 +0000

    Added solution for task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = number % 10
+print_str = "Last digit of {0} is {1} and is".format(number, last_digit)
+if last_digit == 0:
+    print("{} zero".format(print_str))
+else:
+    print(
+        "{0} {1}".format(
+            print_str,
+            ("greater than 5", "less than 6 and not 0")[last_digit <= 5]
+        )
+    )
