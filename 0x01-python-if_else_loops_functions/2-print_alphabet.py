03f8ae8a5cf71c0ca5349321dc4e0eb871682333
Adam Brimer <abrimer@mac.com>
2021-04-27 19:11:33 +0000

    Fix for task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 ascii_arr = list(range(ord('a'), ord('z')))
-abc_str = "".join(map(chr, ascii_arr))
-print(abc_str, end="")
+print("{}".format("".join(map(chr, ascii_arr))), end="")
