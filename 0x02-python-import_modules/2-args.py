d44086b9d4f99ac6659b65a7f65e7a5f06cc8977
Adam Brimer <abrimer@mac.com>
2021-04-27 21:46:04 +0000

    Fix for 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == '__main__':
-    print("{0} argument{1}.".format(len(argv) - 1, ("s", "")[len(argv) == 2]))
+    print("{0} argument{1}{2}".format(len(argv) - 1,
+          ("s", "")[len(argv) == 2], ("", ".")[len(argv) == 1]))
     for ix, a in enumerate(argv[1:]):
         print("{0}: {1}".format(ix + 1, a))
