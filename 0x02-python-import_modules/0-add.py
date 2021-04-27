bda5550616a367824d0bfd6a04d9cad9ece1aabc
Adam Brimer <abrimer@mac.com>
2021-04-27 21:12:03 +0000

    Fix for 0?

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-add = __import__('add_0', globals(), locals(), ['add'])
+math = __import__('add_0', globals(), locals(), ['add'])
+add = math.add
 a = 1
 b = 2
-print("{0} + {1} = {2}".format(a, b, add.add(a, b)))
+print("{0} + {1} = {2}".format(a, b, add(a, b)))
