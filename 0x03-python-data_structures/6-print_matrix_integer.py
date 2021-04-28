2d7a9701d2f1290bb34acefc88fd03bbe41e35d3
Adam Brimer <abrimer@mac.com>
2021-04-28 18:15:24 +0000

    Fix for task 6-- check for is None

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for r in range(len(matrix)):
-        for c in range(len(matrix[r])):
-            print(
-                "{:d}".format(matrix[r][c]),
-                end=(" ",
-                     "\n")[c == len(matrix[r]) - 1])
+    if matrix is not None:
+        for r in range(len(matrix)):
+            if matrix[r] is not None:
+                for c in range(len(matrix[r])):
+                    print(
+                        "{:d}".format(matrix[r][c]),
+                        end=(" ",
+                             "\n")[c == len(matrix[r]) - 1])
