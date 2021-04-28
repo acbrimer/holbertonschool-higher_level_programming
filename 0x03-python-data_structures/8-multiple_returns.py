4ea06083478990de205e8e8a749ea8e727900c5f
Adam Brimer <abrimer@mac.com>
2021-04-28 19:07:26 +0000

    Fix for task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    first_char = sentence[0] if sentence is not None else None
+    first_char = sentence[
+        0] if sentence is not None and len(
+        sentence) > 0 else None
     return (len(sentence), first_char)
