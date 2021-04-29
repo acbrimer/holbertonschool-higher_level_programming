c8918a7eea4874a3f9b80b6217d50631cb112230
Adam Brimer <abrimer@mac.com>
2021-04-29 19:54:27 +0000

    Fix for 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string is None:
-        return (None)
+        return (0)
     numerals = {
         1: ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
         10: ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
