bb8471abd273885c83184f22c33282e64bed09f9
Adam Brimer <abrimer@mac.com>
2021-04-29 18:17:51 +0000

    Added solution for task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    return (
+        sorted(a_dictionary.items(), reverse=True, key=lambda i: i[1])[0][0])
