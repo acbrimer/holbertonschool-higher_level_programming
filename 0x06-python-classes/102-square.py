1336f60b1507e45aad9812ccc32c3048e8c5128b
Adam Brimer <abrimer@mac.com>
2021-05-04 22:14:51 +0000

    Added solution for advanced task 9

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Module containing class definition for Square """
+
+
+class Square:
+
+    """ Square class
+
+        Attributes:
+            size: the size of the square
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+            return self.__size ** 2
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+    def __lt__(self, other):
+        return self.area() < other.area()
+    def __le__(self, other):
+        return self.area() <= other.area()
+    def __gt__(self, other):
+        return self.area() > other.area()
+    def __ge__(self, other):
+        return self.area() >= other.area()
+    def __ne__(self, other):
+        return self.area() != other.area()
