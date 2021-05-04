c1ae66b5dd52e66fe751d333485baedf69a69ebc
Adam Brimer <abrimer@mac.com>
2021-05-04 20:22:46 +0000

    Fix for 7-- handling TypeErrors

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as e:
+    except (TypeError, ValueError) as e:
         print("Exception: {}".format(str(e)), file=sys.stderr)
         return False
         
