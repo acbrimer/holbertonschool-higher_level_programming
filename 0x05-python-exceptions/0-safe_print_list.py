1333ea063b7b257b358e943470d7b3a23a1cb4b9
Adam Brimer <abrimer@mac.com>
2021-05-04 19:18:13 +0000

    Fix for task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     if x == 0:
-        try: 
-            print(my_list[0])
-        except:
-            print()
+        print()
         return (0)
     for i in range(x):
         try:
