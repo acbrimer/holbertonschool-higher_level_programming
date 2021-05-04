2551b760207da2332be5ad7a6c66b02565169a38
Adam Brimer <abrimer@mac.com>
2021-05-04 20:39:08 +0000

    Added solution for task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module containing class definition for Square """
+
+
+class Square:
+
+    """ Square class
+
+        Attributes:
+            size: the size of the square
+    """
+    pass
+
+    def __init__(self, size=0):
+        self.__size = size
