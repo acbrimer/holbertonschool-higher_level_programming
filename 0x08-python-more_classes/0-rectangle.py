c95b5226333b4e751e1842d5d139e026c8bf3951
Adam Brimer <abrimer@mac.com>
2021-05-05 18:30:58 +0000

    Initial commit for 0x08 with solution for task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module containing class definition for Rectangle """
+
+
+class Rectangle:
+
+    """ Rectangle class
+
+        Attributes:
+            None
+    """
+    pass
