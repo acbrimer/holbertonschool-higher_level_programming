9e868c0915f7d52f1cf52bc784513b8d54806cd8
Adam Brimer <abrimer@mac.com>
2021-05-06 05:13:44 +0000

    Added documentation for task 5 and test for no args passed

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" Module for text indentation function """
+
+
 def text_indentation(text):
+    """ Function breaks lines, left aligns between ?.: """
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     t = ".\n\n".join(map(lambda s: s.lstrip(), text.split(".")))
