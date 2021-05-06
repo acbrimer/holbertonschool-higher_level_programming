d451d7c4edf0288ab5b42937a193e1a06455ca0d
Adam Brimer <abrimer@mac.com>
2021-05-06 05:29:03 +0000

    Added remaining tests for task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,15 +12,18 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_negative(self):
         self.assertEqual(max_integer([-1, -2, -3]), -1)
 
+    def test_max_in_the_middle(self):
+        self.assertEqual(max_integer([1,3,5,4,3]), 5)
+
+    def test_max_list_empty(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_max_list_single(self):
+        self.assertEqual(max_integer([1]), 1)
+
     def test_max_not_list(self):
         with self.assertRaises(TypeError):
            max_integer(2)
-
-    def max_in_the_middle(self):
-        self.assertEqual(max_integer([1, 2, 2]), 2)
-
-    def max_list_empty(self):
-        self.assertEqual(max_integer([]), None)
 
 if __name__ == '__main__':
     unittest.main()
