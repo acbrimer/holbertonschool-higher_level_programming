2bb99a4b5b584ec3aef3e3da36fe005e6a04df81
Adam Brimer <abrimer@mac.com>
2021-05-06 04:24:52 +0000

    Added solution and test file for task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module defines function to print square """
+
+
+def print_square(size):
+    """ Function prints a square """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    print("\n".join([("#" * size) for r in range(size)]))
