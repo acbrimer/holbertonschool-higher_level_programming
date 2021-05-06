2be80410397b4ca7b6cc4093475629b54b6e7f1d
Adam Brimer <abrimer@mac.com>
2021-05-06 05:00:02 +0000

    PEP8 fix for 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" Module for add_integer function """
+
+
 def add_integer(a, b=98):
-    """ add_integer-- adds a and b"""
+    """ Function to add a and b"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
