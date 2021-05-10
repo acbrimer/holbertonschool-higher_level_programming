2b9500e517f72b1677d9e8eff4d1622e112fad46
Adam <arbimer@mac.com>
2021-05-10 16:56:44 -0500

    PEP8 fix for 100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.c = (magic_string.c + 1 if 'c' in dir(magic_string) else 1) 
+    magic_string.c = (magic_string.c + 1 if 'c' in dir(magic_string) else 1)
     return(", ".join(["Holberton" for i in range(magic_string.c)]))
