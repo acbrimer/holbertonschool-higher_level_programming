67dfad4c81e6f8ebb515445d0c71b7425e4e1be1
Adam <arbimer@mac.com>
2021-05-10 20:00:58 -0500

    Fix for 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,18 +3,11 @@
 
 class LockedClass:
     """ LockedClass """
-    @property
-    def first_name(self):
-        return self.__first_name
-
-    @first_name.setter
-    def first_name(self, value):
-        self.__first_name = value
-
+    
     def __init__(self, first_name=""):
-        self.__first_name = first_name
+        self.first_name = first_name
 
     def __setattr__(self, key, value):
-        if not key == "first_name":
+        if key != "first_name":
             raise AttributeError( "'LockedClass' object has no attribute '{}'".format(key))
         self.first_name = value
