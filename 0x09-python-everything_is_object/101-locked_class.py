5db7027e30dfd64d9efea697401077767ab5cd5a
Adam <arbimer@mac.com>
2021-05-10 22:49:29 -0500

    Fix for 101-- using __slots__

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,13 +4,4 @@
 
 class LockedClass:
     """ LockedClass """
-
-    def __init__(self, first_name=""):
-        self.first_name = first_name
-        self.__dict__ = None
-
-    def __setattr__(self, key, value):
-        if key != "first_name":
-            raise AttributeError(
-            "'LockedClass' object has no attribute '{}'".format(key))
-        object.__setattr__(self, "first_name", value)
+    __slots__ = ['first_name']
