0d0b59daa02c6afc9f4289b793313c4f6c65ed78
Adam <arbimer@mac.com>
2021-05-10 19:58:51 -0500

    Fix for 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 """ Module for LockedClass"""
 
-@property
-def first_name(self):
-    return self.__first_name
+class LockedClass:
+    """ LockedClass """
+    @property
+    def first_name(self):
+        return self.__first_name
 
-@first_name.setter
-def first_name(self, value):
-    self.__first_name = value
+    @first_name.setter
+    def first_name(self, value):
+        self.__first_name = value
 
-def lc_init(self, first_name=""):
-    self.__first_name = first_name
-    
-def lc_setattr(self, key, value):
-    if not key == "first_name":
-        raise AttributeError( "'LockedClass' object has no attribute '{}'".format(key))
-    first_name(value)
-    
-LockedClass = type("LockedClass", (str, ), {
-    """ LockedClass-- only accepts first_name """
-    "__init__": lc_init,
-    "__setattr__": lc_setattr,
-    "first_name": first_name
-})
+    def __init__(self, first_name=""):
+        self.__first_name = first_name
+
+    def __setattr__(self, key, value):
+        if not key == "first_name":
+            raise AttributeError( "'LockedClass' object has no attribute '{}'".format(key))
+        self.first_name = value
