71381e2ee78cae73eea9e67bd2d863c18e957d4d
Adam Brimer <acbrimer@github.com>
2021-05-11 21:51:33 +0000

    Fix for 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-     Rectangle Module 
+     Rectangle Module
 """
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
+
     """ A four sided shape """
-    __width = 0
-    __height = 0
 
     def __init__(self, width, height):
         super().integer_validator("height", width)
