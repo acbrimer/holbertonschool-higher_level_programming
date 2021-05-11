5d9fcd24694b71484f70177e6cc68b98b945726c
Adam Brimer <acbrimer@github.com>
2021-05-11 21:10:13 +0000

    Added solution for task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module for BaseGeometry """
+
+
+class BaseGeometry:
+    """ Class with area method """
+    def area(self):
+        raise Exception("area() is not implemented")
