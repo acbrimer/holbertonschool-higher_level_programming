1040aa184e5c8cf57a4d73e74f1ff19103a6a378
Adam Brimer <acbrimer@github.com>
2021-05-12 04:08:01 +0000

    Fix for 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,5 +6,5 @@ def add_attribute(self, name, value):
     """ add attribute function """
 
     if not hasattr(self, "__dict__"):
-        raise TypeError("cant't add new attribute")
+        raise TypeError("can't add new attribute")
     self.__setattr__(name, value)
