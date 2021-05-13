44a015fba7255d2dcc8df2ebf80f10faf28d6b48
Adam Brimer <acbrimer@github.com>
2021-05-13 00:44:38 +0000

    Added solution for task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" 
+    Module for to_json_string function
+"""
+import json
+
+def to_json_string(my_obj):
+    """ Function returns json representation of object """
+    return json.dumps(my_obj)
