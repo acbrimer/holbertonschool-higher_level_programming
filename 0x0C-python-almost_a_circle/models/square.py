2e9a627d6c761c8d052068abc6c39f0dad3725e0
Adam Brimer <acbrimer@github.com>
2021-05-17 18:06:48 +0000

    Added update method for square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,6 +13,22 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
         self.size = size
 
+    def update(self, *args, **kwargs):
+        """ Calls update on rectangle with width/height set to size """
+        if len(args) != 0:
+            superargs = list(args)
+            if len(args) > 1:
+                setattr(self, "size", args[1])
+                superargs.insert(1, args[1])
+            super().update(*superargs)
+        else:
+            superargs = dict(kwargs)
+            if "size" in kwargs:
+                setattr(self, "size", kwargs["size"])
+                superargs["width"] = kwargs["size"]
+                superargs["height"] = kwargs["size"]
+            super().update(**superargs)
+
     @property
     def size(self):
         return self.__size
