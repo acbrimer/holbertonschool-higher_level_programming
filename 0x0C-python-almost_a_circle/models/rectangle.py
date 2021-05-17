c62cf999e89123ebc5f5c6bd248667b160a34eb1
Adam Brimer <acbrimer@github.com>
2021-05-17 04:02:23 +0000

    PEP8 fix for Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,4 +83,5 @@ class Rectangle(Base):
     def __str__(self):
         """ Override for string method """
         return "[{}] ({}) {}/{} - {}/{}".format(
-            type(self).__name__, self.id, self.x, self.y, self.width, self.height)
+            type(self).__name__,
+            self.id, self.x, self.y, self.width, self.height)
