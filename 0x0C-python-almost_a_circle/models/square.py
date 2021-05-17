6bf9f3ec0f1b19e115b8274d33015fff4355b380
Adam Brimer <acbrimer@github.com>
2021-05-17 17:37:02 +0000

    Fix for __str__ overload in Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,3 +11,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+        self.__size = size
+
+    def __str__(self):
+        return super().__str__().split("-")[0] + "- {}".format(self.__size)
