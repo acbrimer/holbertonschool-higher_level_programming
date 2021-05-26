3b1efcfbd3f79dccdea40075e5f649cc6a36e011
Adam Brimer <acbrimer@github.com>
2021-05-26 23:32:17 +0000

    Fix for 101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,14 @@
 -- Select cities ordered by avg temp desc
+
 SELECT
-    t.city
-    , AVG(t.value) as "avg_temp"
-FROM temperatures as t
-GROUP BY t.city
-ORDER BY "avg_temp" desc;
+    i.city
+    , i.avg_temp
+FROM 
+(
+    SELECT DISTINCT
+        t.city
+        , AVG(t.value) as avg_temp
+    FROM temperatures as t
+    GROUP BY t.city
+) as i
+ORDER BY i.avg_temp desc;
