c987e5cebade0c9243d9691f643c982c67453412
Adam Brimer <acbrimer@github.com>
2021-05-27 22:37:23 +0000

    Added solution for task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Creates database hbtn_0d_usa and table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+   id int NOT NULL AUTO_INCREMENT PRIMARY KEY
+    , state_id int NOT NULL
+        FOREIGN KEY REFERENCES hbtn_0d_usa.cities(id)
+    , name varchar(256) NOT NULL
+);
