00d2c606a889eb5055fb6c089f44fdd9bc665be9
Adam Brimer <acbrimer@github.com>
2021-05-27 22:39:01 +0000

    Fix for FK in 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
    id int NOT NULL AUTO_INCREMENT PRIMARY KEY
     , state_id int NOT NULL
-        FOREIGN KEY REFERENCES hbtn_0d_usa.cities(id)
+        FOREIGN KEY REFERENCES hbtn_0d_usa.states(id)
     , name varchar(256) NOT NULL
 );
