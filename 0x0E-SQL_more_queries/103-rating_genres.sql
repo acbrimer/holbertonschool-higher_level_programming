f99dd3c43d465ec41331b4446bb3f34201912149
Adam Brimer <acbrimer@github.com>
2021-06-01 15:59:43 +0000

    Added query for 103

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,13 @@
+-- Selects sum of ratings for genres 
+SELECT
+    g.name
+    , SUM(r.rate) AS rating
+FROM tv_genres AS g
+INNER JOIN tv_show_genres AS sg
+    ON g.id = sg.genre_id
+INNER JOIN tv_shows AS s
+    ON sg.show_id = s.id
+LEFT JOIN tv_show_ratings AS r
+    ON s.id = r.show_id
+GROUP BY g.name
+ORDER BY rating DESC;
