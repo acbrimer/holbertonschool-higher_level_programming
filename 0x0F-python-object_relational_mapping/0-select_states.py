c5b375a22b928dc31f773150cc92bb4d0464b3df
Adam Brimer <acbrimer@github.com>
2021-06-01 17:26:42 +0000

    Added solution for task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" 0-select_states """
+import MySQLdb
+import sys
+
+
+def get_connection(**config):
+    """ Safe connection to db with parameters in config """
+    try:
+        db = MySQLdb.connect(**config)
+        return db
+    except Exception as err:
+        print("Error connecting")
+        print(err)
+        print(config)
+        return err
+
+
+def list_states():
+    """ Lists states from db """
+    config = {"host": "localhost", "port": 3306,
+              "user": "root", "passwd": "", "db": "mysql"}
+    if len(sys.argv) == 4:
+        config["user"] = sys.argv[1]
+        config["passwd"] = sys.argv[2]
+        config["db"] = sys.argv[3]
+    db = get_connection(**config)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states;")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+if __name__ == "__main__":
+    list_states()
