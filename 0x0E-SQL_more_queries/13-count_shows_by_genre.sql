a02cfd3b9fdcf215c7449f36352ed82278b66fca
Adam Brimer <acbrimer@github.com>
2021-06-01 15:42:22 +0000

    Added query for 13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Counts shows by genre
+SELECT
+    g.name AS genre
+    , COUNT(sg.show_id) AS number_of_shows
+FROM tv_show_genres AS sg
+INNER JOIN tv_genres AS g
+    ON sg.genre_id = g.id
+GROUP BY g.name
+ORDER BY number_of_shows DESC;
