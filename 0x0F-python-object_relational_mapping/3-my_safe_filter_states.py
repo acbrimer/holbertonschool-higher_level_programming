7001d6961de1702379706f2897682993563dd9a8
Adam Brimer <acbrimer@github.com>
2021-06-01 17:45:29 +0000

    Added solution for task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" 3-my_safe_filter_states """
+import MySQLdb
+import sys
+
+
+def get_connection(**config):
+    """ Safe connection to db with parameters in config """
+    try:
+        db = MySQLdb.connect(**config)
+        return db
+    except Exception as err:
+        print("Error connecting")
+        print(err)
+        print(config)
+        return err
+
+
+def list_states():
+    """ Lists states from db """
+    config = {"host": "localhost", "port": 3306,
+              "user": "root", "passwd": "", "db": "mysql"}
+    p_state = ""
+    if len(sys.argv) == 5:
+        config["user"] = sys.argv[1]
+        config["passwd"] = sys.argv[2]
+        config["db"] = sys.argv[3]
+        p_state = sys.argv[4]
+    db = get_connection(**config)
+    cursor = db.cursor()
+    cursor.execute("""
+        SELECT
+        s.*
+        FROM states AS s
+        WHERE s.name COLLATE latin1_general_cs = '%s'
+        ORDER BY s.id;
+        """ % (p_state))
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+if __name__ == "__main__":
+    list_states()
