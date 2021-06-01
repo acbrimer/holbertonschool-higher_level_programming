08339e4e98d00fba516b6ecd5cf966a3cf9a8fdd
Adam Brimer <acbrimer@github.com>
2021-06-01 15:27:50 +0000

    Fix for 8-- removed state_id from main query

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,6 @@
 -- Selects cities where state_id = cali
 SELECT
     c.id
-    , c.state_id
     , c.name
 FROM cities AS c
     WHERE c.state_id IN (SELECT s.id FROM states AS s WHERE s.name = 'California')
