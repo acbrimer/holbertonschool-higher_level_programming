5779d59b69030830b187ddefa4459c0b088ab9ae
Adam Brimer <acbrimer@github.com>
2021-06-01 15:57:47 +0000

    Added query for 102

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,9 @@
+-- Selects sum of ratings for shows
+SELECT
+    s.title
+    , SUM(r.rate) AS rating
+FROM tv_shows AS s
+LEFT JOIN tv_show_ratings AS r
+    ON s.id = r.show_id
+GROUP BY s.title
+ORDER BY rating DESC;
