db80f199030ce9147f3d9bf0cca32119d7377b01
Adam Brimer <acbrimer@github.com>
2021-06-02 02:36:41 +0000

    Fix for task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-""" 8-model_state_fetch_first """
+""" 9-model_state_filter_a """
 from model_state import Base, State
 import sys
 import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker
 
 
-def get_first_row():
+def get_states():
     """ Fetches top row from states """
     engine = db.create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                               .format(sys.argv[1],
                                       sys.argv[2],
                                       sys.argv[3]
                                       ), pool_pre_ping=True)
-    with engine.connect() as conn:
-        stmt = db.select([State.id, State.name]).limit(1)
-        results = conn.execute(stmt)
-        if not results:
-            print("Nothing")
-        else:
-            for row in results:
-                print("{}: {}".format(row[0], row[1]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    res = session.query(State).limit(1).all()
+    for row in res:
+        print("{}: {}".format(row.id, row.name))
 
 if __name__ == "__main__":
-    get_first_row()
+    get_states()
