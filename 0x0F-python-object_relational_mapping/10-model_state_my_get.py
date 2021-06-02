8d5dbef574faceb16d574b38eadb3de91f8c8559
Adam Brimer <acbrimer@github.com>
2021-06-02 02:45:02 +0000

    Added solution for task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" 9-model_state_filter_a """
+from model_state import Base, State
+import sys
+import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker
+
+
+def get_states():
+    """ Fetches top row from states """
+    engine = db.create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                              .format(sys.argv[1],
+                                      sys.argv[2],
+                                      sys.argv[3]
+                                      ), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    row = session.query(State).filter(State.name == sys.argv[4]).first()
+    if row:
+        print("{}".format(row.id))
+    else:
+        print("Not found")
+
+if __name__ == "__main__":
+    get_states()
