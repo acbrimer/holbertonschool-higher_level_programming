35785ac049b341e83a79f91c4f3a2a1e8f5a5908
Adam Brimer <acbrimer@github.com>
2021-06-03 03:46:25 +0000

    Fix for 7-- handling negative numbers

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,5 +2,7 @@
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing number of occurrences');
 } else {
-  [...Array(parseInt(process.argv[2])).keys()].forEach(k => console.log('C is fun'));
+  if (parseInt(process.argv[2]) > 0) {
+    [...Array(parseInt(process.argv[2])).keys()].forEach(k => console.log('C is fun'));
+  }
 }
