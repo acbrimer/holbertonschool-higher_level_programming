8a03872389f347dad879aeae03235d382167716c
Adam Brimer <acbrimer@github.com>
2021-06-03 03:41:29 +0000

    Fix for 7-- removed hard-coding from array keys

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,5 +2,5 @@
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing number of occurrences');
 } else {
-  [...Array(5).keys()].forEach(k => console.log('C is fun'));
+  [...Array(parseInt(process.argv[2])).keys()].forEach(k => console.log('C is fun'));
 }
