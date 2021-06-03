05a6062dbfd8bc5aa29b3a289ddd32df67db29d2
Adam Brimer <acbrimer@github.com>
2021-06-03 04:08:19 +0000

    Fix for 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@ function factorial (n) {
   if (n === 0 || n === 1) {
     console.log(1);
   } else {
-    console.log([...Array(n).keys()].reverse().reduce((a, c) => a + a * c, 1));
+    console.log(isNaN([...Array(n).keys()].reverse().reduce((a, c) => a + a * c, 1)) ? 'Infinity' : [...Array(n).keys()].reverse().reduce((a, c) => a + a * parseFloat(c), parseFloat(1)));
   }
 }
-factorial(isNaN(parseInt(process.argv[2])) ? 1 : parseInt(process.argv[2]));
+factorial(isNaN(parseInt(process.argv[2])) ? 1 : parseFloat(process.argv[2]));
