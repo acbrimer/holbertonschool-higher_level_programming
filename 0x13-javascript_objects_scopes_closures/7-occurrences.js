37bc544d635b857145f5015925b51f2a8d5bcf86
Adam Brimer <acbrimer@github.com>
2021-06-07 16:17:18 +0000

    Fix for task 7-- .length instead of .count()

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  return list.filter(e => e === searchElement).count();
+  return list.filter(e => e === searchElement).length;
 };
